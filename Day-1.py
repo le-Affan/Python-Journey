989054343f871af1773edf7c0ac208ece263374b
le-Affan <shaikhaffan1234@gmail.com>
2025-06-03 02:06:38 +0530

    Added .gitignore and used append for list

--- Day-1.py
+++ Day-1.py
@@ -1,7 +1,7 @@
-# Create a list of 5 elements and print each element using a loop.
+# Append an item and print new list
 
 desk_items=["Laptop","Mouse","Keyboard","Desk Mat","Lamp"]
-
+desk_items.append("Phone")
 for i in desk_items:
     print (i)
 
