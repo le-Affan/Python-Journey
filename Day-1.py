355857ecf8dfbe9ad5cf61815212ccb4bff594ec
le-Affan <shaikhaffan1234@gmail.com>
2025-06-05 01:11:02 +0530

    Dictionary Basics

--- Day-1.py
+++ Day-1.py
@@ -1,11 +1,23 @@
-# Create a tuple with 4 elements and print it. Then try to modify an element in it.
+# Create a dictionary with 3 student names as keys and their Roll No. as values.
+# Add a new student to the dictionary.
+# Update the Roll no. of one student.
+# Print all the keys and values from the dictionary.
+# Remove one student from the dictionary.
 
-desk_items=("Laptop","Mouse","Keyboard")
 
-for i in desk_items:
-    print(i)
+students = {"Affan":4,
+            "Ankon":14,
+            "Anjali":12}
 
-desk_items[1]="Phone"
+students["Aish"]=7
 
+students["Affan"]=6
+
+print("Keys: ",list(students.keys()))
+print("Values: ",list(students.values()))
+print("Items: ",list(students.items()))
+print(students)
+
+students.pop("Affan")
 
 
