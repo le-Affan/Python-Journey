1c70e89c4d56a57327014c43b2ddc30847627484
le-Affan <shaikhaffan1234@gmail.com>
2025-07-07 19:58:37 +0530

    added some comments

--- OOP_Practice/Basics.py
+++ OOP_Practice/Basics.py
@@ -1,12 +1,12 @@
 class Students:
 
-    graduation_year=2027
+    graduation_year=2027 #-->Class Variables shared across all instances
     program="AI&DS"
     student_count=0
 
-    def __init__(self,name,roll_number):
-        self.name=name
-        self.roll_number=roll_number
+    def __init__(self,name,roll_number): #-->this initialization is run as soon as the class is called by default
+        self.name=name #this line written to remember the value for future use of this object.
+        self.roll_number=roll_number #the value would be forgotten once "init" ends otherwise
         Students.student_count+=1
 
 s1=Students("Affan","16014223006")
